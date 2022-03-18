c52c8653d4f6ae17e122f8b49d74b7f1e5abb5cd
Sebastian <sebastiancurtislavarias@gmail.com>
2022-03-18 14:52:17 +0800

    Item Editor page done

--- src/views/ItemEditor.vue
+++ src/views/ItemEditor.vue
@@ -8,7 +8,99 @@
             </v-col>
 
             <v-col cols="12">
-                <base-text-editor></base-text-editor>
+                <v-row dense>
+                    <v-col cols="12">
+                        <div class="subtitle-1 grey--text mb-1">Section</div>
+                        <v-select
+                            outlined
+                            placeholder="Choose"
+                            dense
+                            color="primary"
+                        ></v-select>
+                    </v-col>
+
+                    <v-col cols="12">
+                        <div class="subtitle-1 grey--text mb-1">Item Name</div>
+                        <v-text-field
+                            outlined
+                            placeholder="Name"
+                            dense
+                            color="primary"
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12">
+                        <div class="subtitle-1 grey--text mb-1">Category</div>
+                        <v-select
+                            outlined
+                            placeholder="Choose"
+                            dense
+                            color="primary"
+                        ></v-select>
+                    </v-col>
+
+                    <v-col cols="12">
+                        <div class="subtitle-1 grey--text mb-1">
+                            Price or Budget
+                        </div>
+                        <v-text-field
+                            type="number"
+                            outlined
+                            placeholder="₱"
+                            dense
+                            color="primary"
+                        >
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col cols="12">
+                        <div class="subtitle-1 grey--text mb-1">Condtion</div>
+                        <v-select
+                            outlined
+                            placeholder="Choose"
+                            dense
+                            color="primary"
+                        ></v-select>
+                    </v-col>
+
+                    <v-col cols="12">
+                        <div class="subtitle-1 grey--text mb-1">Warranty</div>
+                        <v-select
+                            outlined
+                            placeholder="Choose"
+                            dense
+                            color="primary"
+                        ></v-select>
+                    </v-col>
+
+                    <v-col cols="12" class="mb-5">
+                        <div class="subtitle-1 grey--text mb-1">
+                            Description
+                        </div>
+                        <base-text-editor></base-text-editor>
+                    </v-col>
+
+                    <v-col cols="12" class="d-flex">
+                        <v-spacer></v-spacer>
+                        <div>
+                            <v-btn
+                                color="secondary"
+                                class="text-capitalize mr-3"
+                                depressed
+                                >Save
+                                <span class="mx-1 text-lowercase">as</span>
+                                Draft</v-btn
+                            >
+
+                            <v-btn
+                                color="primary"
+                                class="text-capitalize"
+                                depressed
+                                >Post Item</v-btn
+                            >
+                        </div>
+                    </v-col>
+                </v-row>
             </v-col>
         </v-col>
         <v-col cols="12" md="3">
