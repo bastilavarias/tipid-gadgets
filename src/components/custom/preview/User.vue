c65a67000da6349a59cb3f1abcbf211c5c57e693
Sebastian <sebastiancurtislavarias@gmail.com>
2022-05-07 16:09:24 +0800

    Customized user preview in search.

--- src/components/custom/preview/User.vue
+++ src/components/custom/preview/User.vue
@@ -5,10 +5,16 @@
                 <v-img :src="avatar"></v-img>
             </v-list-item-avatar>
             <v-list-item-content>
-                <v-list-item-title class="primary--text font-weight-bold"
+                <v-list-item-title
+                    @click="goToUser(username)"
+                    class="primary--text font-weight-bold pointer"
                     >{{ name }}
                 </v-list-item-title>
-                <v-list-item-subtitle>@{{ username }} </v-list-item-subtitle>
+                <v-list-item-subtitle
+                    @click="goToUser(username)"
+                    class="pointer"
+                    >@{{ username }}
+                </v-list-item-subtitle>
                 <v-list-item-subtitle
                     ><span v-if="location">
                         Location:
@@ -21,23 +27,17 @@
                     </span></v-list-item-subtitle
                 >
             </v-list-item-content>
-            <v-list-item-action>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn color="primary" icon v-bind="attrs" v-on="on">
-                            <v-icon>mdi-message-text</v-icon>
-                        </v-btn>
-                    </template>
-                    <span>Inquire</span>
-                </v-tooltip>
-            </v-list-item-action>
         </v-list-item>
     </v-card>
 </template>
 
 <script>
+import redirectionMixin from '@/mixins/redirection';
+
 export default {
     name: 'user-preview',
+
+    mixins: [redirectionMixin],
 
     props: {
         name: String,
